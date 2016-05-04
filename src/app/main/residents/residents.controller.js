4579b8207e4350692744cdd9b77fdf1bd605149a
nenad <nesa993@gmail.com>
2016-05-04 20:47:03 +0200

    Integrated the mail app into residents and add te list of resident with basic info, added categories and small ui changes

--- src/app/main/residents/residents.controller.js
+++ src/app/main/residents/residents.controller.js
@@ -4,18 +4,239 @@
 
     angular
         .module('app.residents')
-        .controller('ResidentsController', ResidentsController);
+        .controller('MailController', MailController);
 
     /** @ngInject */
-    function ResidentsController()
+    function MailController($scope, $document, $timeout, $mdDialog, $mdMedia, $mdSidenav, Inbox, apilaData)
     {
         var vm = this;
 
         // Data
-        vm.helloText = "Hello residents";
+        vm.accounts = {
+            'creapond'    : 'johndoe@creapond.com',
+            'withinpixels': 'johndoe@withinpixels.com'
+        };
+        vm.checked = [];
+        vm.colors = ['blue-bg', 'blue-grey-bg', 'orange-bg', 'pink-bg', 'purple-bg'];
+        vm.selectedAccount = 'creapond';
+        vm.selectedMail = {};
+        vm.toggleSidenav = toggleSidenav;
+
+        vm.responsiveReadPane = undefined;
+        vm.activeMailPaneIndex = 0;
+        vm.dynamicHeight = false;
+
+        vm.scrollPos = 0;
+        vm.scrollEl = angular.element('#content');
+
+        vm.inbox = Inbox.data;
+        vm.selectedMail = vm.inbox[0];
+        vm.selectedMailShowDetails = false;
 
         // Methods
+        vm.checkAll = checkAll;
+        vm.closeReadPane = closeReadPane;
+        vm.composeDialog = composeDialog;
+        vm.isChecked = isChecked;
+        vm.replyDialog = replyDialog;
+        vm.selectResident = selectResident;
+        vm.toggleStarred = toggleStarred;
+        vm.toggleCheck = toggleCheck;
+
+        //Get data from the api
+        apilaData.residentsList()
+          .success(function(residentList) {
+            console.log(residentList);
+            vm.residentList = residentList;
+          })
+          .error(function(residentList) {
+            console.log("Error retriving the list of residents");
+          });
 
         //////////
+
+        // Watch screen size to activate responsive read pane
+        $scope.$watch(function ()
+        {
+            return $mdMedia('gt-md');
+        }, function (current)
+        {
+            vm.responsiveReadPane = !current;
+        });
+
+        // Watch screen size to activate dynamic height on tabs
+        $scope.$watch(function ()
+        {
+            return $mdMedia('xs');
+        }, function (current)
+        {
+            vm.dynamicHeight = current;
+        });
+
+        /**
+         * Select resident
+         *
+         * @param resident
+         */
+        function selectResident(resident)
+        {
+            console.log(resident);
+
+            vm.selectedMail = resident;
+
+            $timeout(function ()
+            {
+                // If responsive read pane is
+                // active, navigate to it
+                if ( angular.isDefined(vm.responsiveReadPane) && vm.responsiveReadPane )
+                {
+                    vm.activeMailPaneIndex = 1;
+                }
+
+                // Store the current scrollPos
+                vm.scrollPos = vm.scrollEl.scrollTop();
+
+                // Scroll to the top
+                vm.scrollEl.scrollTop(0);
+            });
+        }
+
+        /**
+         * Close read pane
+         */
+        function closeReadPane()
+        {
+            if ( angular.isDefined(vm.responsiveReadPane) && vm.responsiveReadPane )
+            {
+                vm.activeMailPaneIndex = 0;
+
+                $timeout(function ()
+                {
+                    vm.scrollEl.scrollTop(vm.scrollPos);
+                }, 650);
+            }
+        }
+
+        /**
+         * Toggle starred
+         *
+         * @param mail
+         * @param event
+         */
+        function toggleStarred(mail, event)
+        {
+            event.stopPropagation();
+            mail.starred = !mail.starred;
+        }
+
+        /**
+         * Toggle checked status of the mail
+         *
+         * @param mail
+         * @param event
+         */
+        function toggleCheck(mail, event)
+        {
+            if ( event )
+            {
+                event.stopPropagation();
+            }
+
+            var idx = vm.checked.indexOf(mail);
+
+            if ( idx > -1 )
+            {
+                vm.checked.splice(idx, 1);
+            }
+            else
+            {
+                vm.checked.push(mail);
+            }
+        }
+
+        /**
+         * Return checked status of the mail
+         *
+         * @param mail
+         * @returns {boolean}
+         */
+        function isChecked(mail)
+        {
+            return vm.checked.indexOf(mail) > -1;
+        }
+
+        /**
+         * Check all
+         */
+        function checkAll()
+        {
+            if ( vm.allChecked )
+            {
+                vm.checked = [];
+                vm.allChecked = false;
+            }
+            else
+            {
+                angular.forEach(vm.inbox, function (mail)
+                {
+                    if ( !isChecked(mail) )
+                    {
+                        toggleCheck(mail);
+                    }
+                });
+
+                vm.allChecked = true;
+            }
+        }
+
+        /**
+         * Open compose dialog
+         *
+         * @param ev
+         */
+        function composeDialog(ev)
+        {
+            $mdDialog.show({
+                controller         : 'ComposeDialogController',
+                controllerAs       : 'vm',
+                locals             : {
+                    selectedMail: undefined
+                },
+                templateUrl        : 'app/main/residents/dialogs/compose/compose-dialog.html',
+                parent             : angular.element($document.body),
+                targetEvent        : ev,
+                clickOutsideToClose: true
+            });
+        }
+
+        /**
+         * Open reply dialog
+         *
+         * @param ev
+         */
+        function replyDialog(ev)
+        {
+            $mdDialog.show({
+                controller         : 'ComposeDialogController',
+                controllerAs       : 'vm',
+                locals             : {
+                    selectedMail: vm.selectedMail
+                },
+                templateUrl        : 'app/main/residents/dialogs/compose/compose-dialog.html',
+                parent             : angular.element($document.body),
+                targetEvent        : ev,
+                clickOutsideToClose: true
+            });
+        }
+
+        /**
+         * Toggle sidenav
+         *
+         * @param sidenavId
+         */
+        function toggleSidenav(sidenavId)
+        {
+            $mdSidenav(sidenavId).toggle();
+        }
     }
 })();
